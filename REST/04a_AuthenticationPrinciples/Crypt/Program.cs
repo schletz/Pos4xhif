dc839707eefa0211721492c670d319d84c23de8f
schletz <schletz@gmail.com>
2019-10-14 10:50:27 +0200

    Commit 14/10/2019

---
+++ REST/04a_AuthenticationPrinciples/Crypt/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace Crypt
+{
+    /// <summary>
+    /// Demoprogramm für Hashalgorithmen. Start mit dotnet run.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 32stellige GUID als Hexstring als Salt generieren
+            string salt = GenerateSalt();
+
+            // Nun wird das Passwort mit dem Salt versehen und der SHA256 Hashwert berechnet:
+            string myPassword = "1234";
+            string saltedPassword = CalculateHash(myPassword, salt);
+        }
+
+        /// <summary>
+        /// Generiert eine 128bit lange Zufallszahl und gibt sie Base64Codiert (24 Stellen) in der Form
+        /// nmU2xPjixsbAKqblq59NNg==
+        /// zurück.
+        /// </summary>
+        private static string GenerateSalt(int bits = 128)
+        {
+            // 128bit Salt erzeugen.
+            byte[] salt = new byte[bits / 8];
+            using (System.Security.Cryptography.RandomNumberGenerator rnd = System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                rnd.GetBytes(salt);
+            }
+            return Convert.ToBase64String(salt);
+        }
+
+        /// <summary>
+        /// Verknüpft das übergebene Salt mit dem Passwort und berechnet aufgrund der
+        /// UTF8 Werte der Zeichen die SHA256 Prüfsumme. Gibt den Wert Base64 codiert (44 Stellen)
+        /// in der Form 
+        /// mswEwk+8U0rDGstvICGb5AhycUjw+si2PypAISs0U6Q=
+        /// zurück.
+        /// </summary>    
+        private static string CalculateHash(string password, string salt)
+        {
+            byte[] saltBytes = Convert.FromBase64String(salt);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            System.Security.Cryptography.HMACSHA256 myHash = new System.Security.Cryptography.HMACSHA256(saltBytes);
+
+            byte[] hashedData = myHash.ComputeHash(passwordBytes);
+
+            // Das Bytearray wird Base64 codiert zurückgegeben.
+            string hashedPassword = Convert.ToBase64String(hashedData);
+            Console.WriteLine($"Salt:            {salt}");
+            Console.WriteLine($"Password:        {password}");
+            Console.WriteLine($"Hashed Password: {hashedPassword}");              
+            return hashedPassword;
+        }
+        /// <summary>
+        /// Prüft, ob das übergebene Passwort mit dem gespeicherten Hashwert und dem Salt übereinstimmt.
+        /// </summary>     
+        static bool CheckPassword(string password, string salt, string hashedPassword) =>
+            hashedPassword == CalculateHash(password, salt);
+
+    }
+}
--- REST/04a_AuthenticationPrinciples/README.md
+++ REST/04a_AuthenticationPrinciples/README.md
@@ -56,6 +56,23 @@ Würde Salt und Passwort nämlich wie vorher erwähnt mit einer einfachen String
 werden, ergeben sich Kollisionen ("AB" + "CD" = "ABC" + "D"). Mit HMAC wird dieses Verknüpfen 
 durch bitweise Operationen durchgeführt und ist daher besser als die Stringverknüpfung.
 
+> HMACSHA256 is a type of keyed hash algorithm that is constructed from the SHA-256 hash function 
+> and used as a Hash-based Message Authentication Code (HMAC). The HMAC process mixes a  secret key 
+> with the message data, hashes the result with the hash function, mixes that hash value with the 
+> secret key again, and then applies the hash function a second time. The output hash is 256 bits in 
+> length.
+> 
+> An HMAC can be used to determine whether a message sent over an insecure channel has been tampered 
+> with, provided that the sender and receiver share a secret key. The sender computes the hash value 
+> for the original data and sends both the original data and hash value as a single message. The 
+> receiver recalculates the hash value on the received message and checks that the computed HMAC 
+> matches the transmitted HMAC.
+> 
+> Any change to the data or the hash value results in a mismatch, because knowledge of the secret 
+> key is required to change the message and reproduce the correct hash value. Therefore, if the 
+> original and computed hash values match, the message is authenticated. 
+> (https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.hmacsha256?view=netframework-4.8)
+
 Folgendes Codebeispiel generiert einen 128bit Salt Wert und berechnet den Hashwert des Passwortes
 "1234":
 ```c#
