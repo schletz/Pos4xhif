77848744f851f8b1311ab5a5203885ada3ac930b
Michael Schletz <schletz@gmail.com>
2019-12-22 10:52:21 +0100

    Commit 22/12/2019

--- REST/03_EntityFrameworkCore/Model/TestsContext.cs
+++ REST/03_EntityFrameworkCore/Model/TestsContext.cs
@@ -24,11 +24,6 @@ namespace EntityFrameworkCore.Model
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlite("DataSource=Tests.db");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- REST/03_EntityFrameworkCore/Startup.cs
+++ REST/03_EntityFrameworkCore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +27,8 @@ namespace EntityFrameworkCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<Model.TestsContext>();
+            services.AddDbContext<Model.TestsContext>(options =>
+                options.UseSqlite(Configuration["AppSettings:Database"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
