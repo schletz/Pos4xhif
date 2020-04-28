ad2fd5c495f3e1b0fc50ca19de5bd46a2bd6d3f6
Michael Schletz <schletz@gmail.com>
2020-04-28 06:46:29 +0200

    Commit 20200428

---
+++ XAMARIN/10_StateManagement/StateDemo/StateDemo/StateDemo/ViewModel/DetailModel.cs
@@ -0,0 +1,24 @@
+﻿using StateDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace StateDemo.ViewModel
+{
+    class DetailModel : INotifyPropertyChanged
+    {
+        public string Username { get; set; }
+        public DetailModel()
+        {
+            // Das UserChanged Event des StateManegers abonnieren und die Logik im VM ausführen.
+            StateManager.Instance.UserChanged += (username) =>
+             {
+                 Username = username;
+                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Username)));
+             };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
