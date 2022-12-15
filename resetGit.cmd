db9e406fd6beed00af21511b26cfa510097554b8
schletz <schletz@spengergasse.at>
2022-12-15 08:52:07 +0100

    Commit 20221215

--- resetGit.cmd
+++ resetGit.cmd
@@ -2,6 +2,11 @@
 chcp 65001
 echo Achtung: Alle lokalen Änderungen werden zurückgesetzt. Drücke CTRL+C zum Abbrechen.
 pause
-git add -A
-git fetch --all
-git reset --hard origin/master
+
+git fetch --all --prune
+FOR /F "tokens=* delims=* " %%a IN ('git branch') DO (
+    echo Reset branch %%a...
+    git checkout %%a
+    git clean -df
+    git reset --hard origin/%%a
+)
